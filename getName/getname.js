269a299cdba823ede3f4ebe84fe7213693c4f270
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-19 16:50:59 -0700

    refactored function to destructure objects name, and tests remain green

--- getName/getname.js
+++ getName/getname.js
@@ -1,6 +1,8 @@
 const getName = (object) => {
-  //fucntion needs to pass in an object and return the name of the object.
-  return object.name;
+  //fucntion needs to pass in an object and return the name of the object
+  const { name } = object;
+
+  return name;
 };
 
 export default getName;
