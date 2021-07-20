762c17ce56cb20c02cd744d4934d655129917866
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-07-19 18:32:04 -0700

    updated code in copy push function to use spread syntax in order to copy and append

--- copyandpush/copyandpush.js
+++ copyandpush/copyandpush.js
@@ -1,7 +1,9 @@
 const copyAndPush = (numbers, n) => {
   //fucntion needs to pass in an array of numbers and return a new array with the numbers in original array and a new number added to the end
-  const newArray = numbers;
-  newArray.push(n);
+  const numbersArray = numbers;
+  const newNumber = n;
+
+  const newArray = [...numbersArray, newNumber];
 
   return newArray;
 };
